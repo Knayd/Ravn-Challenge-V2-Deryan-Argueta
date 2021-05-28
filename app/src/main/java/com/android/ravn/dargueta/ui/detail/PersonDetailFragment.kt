0d933e8e13e1d20a8d9d225b436a23db85b4d76c
Deryan Argueta <deryan.hndz@gmail.com>
2021-05-27 22:01:16 -0600

    :construction: Adding blank fragments.

---
+++ app/src/main/java/com/android/ravn/dargueta/ui/detail/PersonDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.android.ravn.dargueta.ui.detail
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import com.android.ravn.dargueta.R
+import com.android.ravn.dargueta.base.BaseFragment
+import com.android.ravn.dargueta.databinding.FragmentPersonDetailBinding
+
+class PersonDetailFragment : BaseFragment<FragmentPersonDetailBinding>(
+    R.layout.fragment_person_detail
+) {
+    override val viewModel: ViewModel? = null
+
+    override fun onFragmentReady(savedInstanceState: Bundle?) {
+
+    }
+
+}
---
+++ app/src/main/java/com/android/ravn/dargueta/ui/list/PeopleListFragment.kt
@@ -0,0 +1,19 @@
+package com.android.ravn.dargueta.ui.list
+
+import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import com.android.ravn.dargueta.R
+import com.android.ravn.dargueta.base.BaseFragment
+import com.android.ravn.dargueta.databinding.FragmentPeopleListBinding
+
+
+class PeopleListFragment : BaseFragment<FragmentPeopleListBinding>(
+    R.layout.fragment_people_list
+) {
+
+    override val viewModel: ViewModel? = null
+
+    override fun onFragmentReady(savedInstanceState: Bundle?) {
+
+    }
+}
