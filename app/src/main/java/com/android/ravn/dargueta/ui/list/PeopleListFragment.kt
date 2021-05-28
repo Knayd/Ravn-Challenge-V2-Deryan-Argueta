cf41ca01b86382c2e27ad3570733877fc174c2ef
Deryan Argueta <deryan.hndz@gmail.com>
2021-05-28 17:18:15 -0600

    :lipstick: Adding recyclerview to fragment.

--- app/src/main/java/com/android/ravn/dargueta/ui/list/PeopleListFragment.kt
+++ app/src/main/java/com/android/ravn/dargueta/ui/list/PeopleListFragment.kt
@@ -1,24 +1,21 @@
 package com.android.ravn.dargueta.ui.list
 
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.android.ravn.dargueta.R
 import com.android.ravn.dargueta.base.BaseFragment
 import com.android.ravn.dargueta.databinding.FragmentPeopleListBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class PeopleListFragment : BaseFragment<FragmentPeopleListBinding>(
     R.layout.fragment_people_list
 ) {
 
-    override val viewModel: ViewModel? = null
+    override val viewModel: PeopleListViewModel by viewModels()
 
     override fun onFragmentReady(savedInstanceState: Bundle?) {
-        binding.tvList.setOnClickListener {
-            findNavController().navigate(
-                PeopleListFragmentDirections.toPersonDetailFragment()
-            )
-        }
+
     }
 }
