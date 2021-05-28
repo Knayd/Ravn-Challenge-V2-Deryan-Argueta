0095b60a23bfa376ef079cc8d7cb016c104f6855
Deryan Argueta <deryan.hndz@gmail.com>
2021-05-27 22:24:22 -0600

    :sparkles: Adding basic navigation component implementation.

--- app/src/main/java/com/android/ravn/dargueta/MainActivity.kt
+++ app/src/main/java/com/android/ravn/dargueta/MainActivity.kt
@@ -2,10 +2,21 @@ package com.android.ravn.dargueta
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment).let { navHostFragment ->
+            val navController = navHostFragment.navController
+            val config = AppBarConfiguration(navController.graph)
+            findViewById<Toolbar>(R.id.toolbar_main)
+                .setupWithNavController(navController, config)
+        }
     }
 }
--- app/src/main/java/com/android/ravn/dargueta/ui/list/PeopleListFragment.kt
+++ app/src/main/java/com/android/ravn/dargueta/ui/list/PeopleListFragment.kt
@@ -2,6 +2,7 @@ package com.android.ravn.dargueta.ui.list
 
 import android.os.Bundle
 import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
 import com.android.ravn.dargueta.R
 import com.android.ravn.dargueta.base.BaseFragment
 import com.android.ravn.dargueta.databinding.FragmentPeopleListBinding
@@ -14,6 +15,10 @@ class PeopleListFragment : BaseFragment<FragmentPeopleListBinding>(
     override val viewModel: ViewModel? = null
 
     override fun onFragmentReady(savedInstanceState: Bundle?) {
-
+        binding.tvList.setOnClickListener {
+            findNavController().navigate(
+                PeopleListFragmentDirections.toPersonDetailFragment()
+            )
+        }
     }
 }
