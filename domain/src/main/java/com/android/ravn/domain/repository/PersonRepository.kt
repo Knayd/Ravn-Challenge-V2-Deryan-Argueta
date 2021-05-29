19dd79d4900f0d836e71d45372679428c0acc93e
Deryan Argueta <deryan.hndz@gmail.com>
2021-05-28 23:15:30 -0600

    :recycle: Fixing repository return type.

--- domain/src/main/java/com/android/ravn/domain/repository/PersonRepository.kt
+++ domain/src/main/java/com/android/ravn/domain/repository/PersonRepository.kt
@@ -1,8 +1,9 @@
 package com.android.ravn.domain.repository
 
+import androidx.paging.PagingData
 import com.android.ravn.domain.model.Person
 import kotlinx.coroutines.flow.Flow
 
 interface PersonRepository {
-    fun getPeople(): Flow<List<Person>>
+    fun getPeople(): Flow<PagingData<Person>>
 }
