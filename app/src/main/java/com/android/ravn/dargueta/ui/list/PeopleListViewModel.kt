8c63e89ec0ca744b46d20764dc0df8e83a06e7af
Deryan Argueta <deryan.hndz@gmail.com>
2021-05-29 14:26:37 -0600

    :bug: Adding missing method invocation to avoid crash while going back.

--- app/src/main/java/com/android/ravn/dargueta/ui/list/PeopleListViewModel.kt
+++ app/src/main/java/com/android/ravn/dargueta/ui/list/PeopleListViewModel.kt
@@ -2,6 +2,8 @@ package com.android.ravn.dargueta.ui.list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.android.ravn.domain.interaction.person.GetPeopleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -11,5 +13,5 @@ class PeopleListViewModel @Inject constructor(
     getPeopleUseCase: GetPeopleUseCase
 ) : ViewModel() {
 
-    val people = getPeopleUseCase().asLiveData()
+    val people = getPeopleUseCase().cachedIn(viewModelScope).asLiveData()
 }
